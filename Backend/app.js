fb149667721fb310dee33c898557547480c52dd1
Arjun <130980602+arjunisonline@users.noreply.github.com>
2025-09-18 19:27:28 +0530

    updated cors

--- Backend/app.js
+++ Backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin: 'http://localhost:5080', credentials: true }));
+app.use(cors({ origin: 'https://task-manager-1-a6hb.onrender.com', credentials: true }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
